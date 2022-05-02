c065b9a98c6df6da956e871223b4d061ca864275
cmjiwa <jinghuan.wang@tuni.fi>
2022-05-02 14:20:11 +0300

    destructuring

---
+++ destructuring.js
@@ -0,0 +1,16 @@
+// destructuring has two types
+//1. array destructuring
+
+const [a, b] = ['rose', 'ben'];
+console.log(a, b);
+
+//2. object destructuring
+const {age, nam} = {nam: 'rose', age: 90};
+console.log(nam);
+console.log(age);
+
+
+/*
+Spread operator is to get all the elements or properties, 
+	Destructuring is to get single elements or properties and store them in variables for arrays and objects
+*/
