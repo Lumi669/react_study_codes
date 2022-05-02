a54faf62e902599de4c9259ff0cec59f46a8e901
cmjiwa <jinghuan.wang@tuni.fi>
2022-05-02 11:44:10 +0300

    class : use extends keyword to inherit and add  to the constructor of child class

--- classExample.js
+++ classExample.js
@@ -1,5 +1,21 @@
-class Person {
+//create a second class
+class Human {
+    constructor(){
+        this.gender = 'male';
+    }
+
+    printGender(){
+        console.log(this.gender);
+    }
+}
+
+//create a class
+class Person extends Human{
+    //use contructor function to set up properties
     constructor(){  
+        //execute parent constructor
+        super();
+        //use keyword this to set up properties
         this.name = 'Max';       
     }
 
@@ -8,8 +24,11 @@ class Person {
     }
 }
 
+//create an instance of class Person with keyword new
 const person = new Person();
 person.printMyName();
+
+person.printGender();
 
 
 
@@ -22,5 +41,9 @@ Note:
 2. inside constructor, use `this` keyword to set up properties
 
 3. we can use class in store an instance in a constant with a new person and then we can execute person.p.rintMyName()
+
+4. we can let Person inherit Human
+ - use keyword extends
+ - in Person constructuor, add super() to execute parent constructor
 
 */
